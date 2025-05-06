56ee4886c9bf1dbd113de4056760e2f4a41bc72d
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-05-06 10:07:10 -0500

    subir imagen al servidor, subir imagen y actualizar los datos del usurio con imagen URL

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
@@ -30,6 +31,24 @@ export class UsersController {
   update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
+
+
+  @Post('upload/:id')
+  @UseInterceptors(FileInterceptor('file'))
+  updateWithImage(@UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5MB
+        new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+      ],
+    }),
+  ) file: Express.Multer.File,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto) {
+    // console.log(file);
+    return this.usersService.updateWithImage(file, id, updateUserDto);
+  }
+
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import storage = require('../utils/cloud_storage');
 
 @Injectable()
 export class UsersService {
@@ -34,6 +35,24 @@ export class UsersService {
     }
     const userUpdated = Object.assign(userFound, user);
     return this.usersRepository.save(userUpdated);
+  }
+
+  async updateWithImage(file: Express.Multer.File, id: number, user: UpdateUserDto) {
+    const url = await storage(file, file.originalname);
+    console.log('URL: ' + url);
+
+    if (url === undefined && url === null) {
+      return new HttpException('La imagen no se pudo guardar', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    const userFound = await this.usersRepository.findOneBy({ id: id });
+    if (!userFound) {
+      return new HttpException('Usuario no Existe', HttpStatus.NOT_FOUND);
+    }
+    user.image = url;
+    const userUpdated = Object.assign(userFound, user);
+    return this.usersRepository.save(userUpdated);
+
   }
 
   remove(id: number) {
